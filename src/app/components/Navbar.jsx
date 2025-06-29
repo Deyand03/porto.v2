2cf660e02556d040eed066d3936529ba4ed8aa46
Deyand03 <defryyandy7@gmail.com>
2025-06-29 20:29:23 +0700

    add shadcn

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between items-center p-5 bg-gray-900/80 text-white'>
+        <div>=</div>
+        <div>Deyporto</div>
+        <div></div>
+    </nav>
+  )
+}
+
+export default Navbar
